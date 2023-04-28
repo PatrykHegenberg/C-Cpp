0cf056bab761e5fdea441b24ce8857b466a13ed8
PatrykHegenberg <patrykhegenberg@gmail.com>
2023-04-28 11:14:07 +0200

    add exercises on datatypes

---
+++ lecture1/02-Datentypen/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main() {
+  // Boolean Werte
+  // bool istok;
+  // istok = true;
+
+  // std::cout << "der Wert von istok ist " << istok << std::endl;
+  // std::cout << "die Groesse (in bytes) von istok ist " << sizeof(istok) << std::endl;
+
+  // if(istok) {
+  //   std::cout << "istok ist wahr" <<std::endl;
+  // } else {
+  //   std::cout << "istok ist falsch" <<std::endl;
+  // }
+  
+  // Caharacterwerte
+  // char zeichen;
+  // zeichen = 'A';
+
+  // std::cout << "der Wert von zeichen ist:" << zeichen <<std::endl;
+  // std::cout << "Die Groesse (in Bytes) von zewichen ist:" << sizeof(zeichen) << std::endl;
+
+  // if(zeichen) {
+  //   std::cout << "zeichen ist wahr" << std::endl;
+  // } else {
+  //   std::cout << "zeichen ist falsch" << std::endl;
+  // }
+  
+  // Beispiele für Ganzzahlen
+
+  // int n;
+  // n = -100;
+  // std::cout << "Der Wert von n ist: " << n <<std:endl;
+  // std::cout << "Die Groesse (in bytes) von n ist:" << sizeof(n) << std::endl;
+  
+  // Strings
+  std::string str;
+  str = "Hallo Welt";
+  std::cout << "Der Wert von str ist: " << str << std::endl;
+  std::cout << "Die Groesse (in bytes) von str ist: " << sizeof(str) << std::endl;
+
+}
