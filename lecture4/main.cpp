b9d867ebab40999700ac8862528396c59363cf5a
PatrykHegenberg <patrykhegenberg@gmail.com>
2023-06-16 14:50:33 +0200

    2023-06-16 After lecture

--- lecture4/main.cpp
+++ lecture4/main.cpp
@@ -5,12 +5,22 @@
 #include <vector>
 #include <chrono>
 
-void up_and_down(std::vector<std::string> v, bool up) {
-  for (auto it : v) {
+void up_and_down_value(std::vector<std::string> v, bool up) {
+  for (auto i= 0; i < v.size(); i++) {
     if (up) {
-      v.push_back("a");
+      v[i].push_back('a');
     } else {
-      v.pop_back();
+      v[i].pop_back();
+    }
+  }
+}
+
+void up_and_down_ref(std::vector<std::string>& v, bool up) {
+  for (auto i= 0; i < v.size(); i++) {
+    if (up) {
+      v[i].push_back('a');
+    } else {
+      v[i].pop_back();
     }
   }
 }
@@ -22,13 +32,26 @@ int main() {
     auto start = std::chrono::high_resolution_clock::now();
   for (auto i = 0; i <= 1000; ++i) {
     if (i % 2 == 0) {
-      up_and_down(text, true);
+      up_and_down_value(text, true);
     } else {
-      up_and_down(text, false);
+      up_and_down_value(text, false);
     }
   }
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
     std::cout << "Time: " << duration.count() << std::endl;
+
+    auto start1 = std::chrono::high_resolution_clock::now();
+  for (auto i = 0; i <= 1000; ++i) {
+    if (i % 2 == 0) {
+      up_and_down_ref(text, true);
+    } else {
+      up_and_down_ref(text, false);
+    }
+  }
+    auto stop1 = std::chrono::high_resolution_clock::now();
+    auto duration1 = std::chrono::duration_cast<std::chrono::microseconds>(stop1 - start1);
+    std::cout << "Time: " << duration1.count() << std::endl;
+    std::cout << "Verhältnis: " << duration / duration1 << std::endl;
   return 0;
 }
